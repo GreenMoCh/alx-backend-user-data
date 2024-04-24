be1a1d97bab44f83fd601873256bbf7ffec6de80
GreenMoCh <125460183+GreenMoCh@users.noreply.github.com>
2024-04-24 22:09:31 +0100

    x03. User authentication service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -32,6 +32,87 @@ def users():
     except ValueError as err:
         response = {"message": str(err)}
         return jsonify(response), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """
+    Login route to create a new session for the user
+    """
+    data = request.form
+    email = data.get("email")
+    password = data.get("password")
+    if email and password:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie("session_id", session_id)
+            return response
+    
+    return "Unauthorized", 401
+
+
+@app.route("/sessions", methods=['DELETE'])
+def logout():
+    """
+    Logout route
+    """
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/")
+
+    return "Forbidden", 403
+
+
+@app.route("/profile")
+def profile():
+    """
+    Get data of user profile
+    """
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user =  AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email})
+    
+    return "Forbiden", 403
+
+
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    if request.method == "POST":
+        data = request.form
+        if "email" not in data:
+            abort(400)
+
+        email = data["email"]
+        try:
+            reset_token = AUTH.get_reset_password_token(email)
+            return jsonify({"email": email, "reset_token": reset_token}), 200
+        except ValueError as e:
+            abort(403, str(e))
+
+
+@app.route("/reset_password", methods=['PUT'])
+def update_password():
+    """
+    Update password
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if not email or not reset_token or not new_password:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,3 +53,70 @@ class Auth:
             return bcrypt.checkpw(password.encode('utf-8'), user.hased_password)
         except:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Create a session for the user and return the session ID
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate.uuid()
+            user.session_id = session_id
+            self._db._session.commit()
+
+            return session_id
+        except:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        GEt user corresponding to the session ID
+        """
+        try:
+            if session_id is None:
+                return None
+
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroy the session for the user
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+            user.session_id = None
+            self._db._session.commit()
+        except:
+            pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Get reset password token
+        """
+        user = self._db.find_user_by(email=email)
+        if not user:
+            raise ValueError
+        reset_token = str(uuid.uuid4())
+        user.reset_token = reset_token
+        self._db.commit()
+        return reset_token
+
+    def update_password(self, reset_token, password):
+        """
+        Updates password using reset_token
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except:
+            raise ValueError
+
+        hashed_password = self._hash_password(password)
+
+        user.hased_password = hashed_password
+        user.reset_token = None
+
+        self._db._session.commit()
+        
